572e0e379a8b1a8b176e168075a5ce743443b994
pamplona-c <gustavo.pamplona.549@gmail.com>
2023-08-20 19:03:23 -0300

    arquivo principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
+//import {AiOutlineShoppingCart} from 'react-icons/ai';
+import Header from './components/Header/header';
+import Products from './components/products/products';
 
 
 function App() {
+	
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-          Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-          Learn React
-				</a>
-			</header>
+		<div>
+			<Header/>
+			<Products/>
 		</div>
 	);
 }
